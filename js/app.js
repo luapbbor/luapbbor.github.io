a564f0a69526df5a6112dd483ae7f8cffcb75021
luapbbor <luapbbor@gmail.com>
2020-08-31 15:21:06 +1000

    Intro animations complete

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// const downArrow = document.querySelector('.ct-js-btn-scroll');
+// downArrow.style.display = 'none';
+
+// function showArrow(){
+//     downArrow.style.display = 'inline-block';
+// }
+
+// setTimeout(showArrow, 3000);
